680cc78acd695969a1025f0d5b8bf9ff8c459196
Emmapawlowska <emma.pawlowska@thinkingzone.pl>
2024-12-07 17:58:46 +0100

    Update zadanie7.py
    done

--- zadanie7.py
+++ zadanie7.py
@@ -1,20 +1,18 @@
 import random
 
-# Losowanie, czy pada (True - pada, False - nie pada)
+
 pada = random.choice([True, False])
 
 zgaduj = True
 
-# Pętla będzie trwała dopóki użytkownik nie zgadnie poprawnie
+
 while zgaduj:
-    odpowiedz = input("Czy pada? (t/n): ").lower()  # Pobieranie odpowiedzi użytkownika, zamieniamy na małe litery
-    
-    # Sprawdzanie, czy użytkownik odpowiedział "t" (true) lub "n" (false)
+    odpowiedz = input("Czy pada? (t/n): ").lower()  
     if odpowiedz == 't' and pada:
         print("Brawo! Zgadłeś, pada!")
-        zgaduj = False  # Zgadnięto poprawnie, kończymy pętlę
+        zgaduj = False  
     elif odpowiedz == 'n' and not pada:
         print("Brawo! Zgadłeś, nie pada!")
-        zgaduj = False  # Zgadnięto poprawnie, kończymy pętlę
+        zgaduj = False  
     else:
         print("Źle! Spróbuj ponownie.")
