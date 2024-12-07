188c96250298e0c95121492d3e6b3546da696194
Emmapawlowska <emma.pawlowska@thinkingzone.pl>
2024-12-07 18:00:36 +0100

    Update zadanie5.py
    done

--- zadanie5.py
+++ zadanie5.py
@@ -1,6 +1,6 @@
 suma = 0
 
-for i in range(1, 101, 2):  # zaczynamy od 1, krok 2 (liczby nieparzyste)
+for i in range(1, 101, 2):  
     suma += i
 
 print("Suma liczb nieparzystych od 1 do 100 wynosi:", suma)
